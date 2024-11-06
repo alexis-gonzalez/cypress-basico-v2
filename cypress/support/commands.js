9b8b2f4291d76786faa8ff6f57eaf238686a1d63
Alexis Gonzalez <alxs.gonzalez@gmail.com>
2024-11-06 18:54:55 -0300

    Novas atualizações

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
+    cy.get('#firstName')
+            .should('be.visible')
+            .type('Alexis')
+            .should('have.value', 'Alexis');
+        cy.get('#lastName')
+            .should('be.visible')
+            .type('Gonzalez')
+            .should('have.value', 'Gonzalez');
+        cy.get('#email')
+            .should('be.visible')
+            .type('alxs.gonzalez@gmail.com')
+            .should('have.value', 'alxs.gonzalez@gmail.com');
+        cy.get('#open-text-area')
+            .should('be.visible')
+            .type('Teste');
+        cy.get('button[type="submit"]')
+            .click();
+        cy.get('.success')
+            .should('be.visible');
+})
